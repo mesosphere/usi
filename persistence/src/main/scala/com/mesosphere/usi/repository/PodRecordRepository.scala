b861462f7766f4d1578379f844fc9dc15fcd0f95
Nikita Melkozerov <nmelkozerov@mesosphere.io>
2019-01-29 16:20:27 +0100

    docs

--- persistence/src/main/scala/com/mesosphere/usi/repository/PodRecordRepository.scala
+++ persistence/src/main/scala/com/mesosphere/usi/repository/PodRecordRepository.scala
@@ -4,10 +4,23 @@ import com.mesosphere.usi.core.models.{PodId, PodRecord}
 
 import scala.concurrent.Future
 
+/**
+  * Repository that stores [[PodRecord]].
+  */
 trait PodRecordRepository {
 
+  /**
+    * Stores the provided [[PodRecord]] in the repository.
+    * @param record
+    * @return
+    */
   def store(record: PodRecord): Future[Unit]
 
+  /**
+    * Retrieves the [[PodRecord]] if it exists.
+    * @param podId
+    * @return
+    */
   def find(podId: PodId): Future[Option[PodRecord]]
 
 }
--- persistence/src/main/scala/com/mesosphere/usi/repository/ReservationRecordRepository.scala
+++ persistence/src/main/scala/com/mesosphere/usi/repository/ReservationRecordRepository.scala
@@ -1,13 +1,26 @@
 package com.mesosphere.usi.repository
 
-import com.mesosphere.usi.core.models.{ReservationId, ReservationRecord}
+import com.mesosphere.usi.core.models.{PodRecord, ReservationId, ReservationRecord}
 
 import scala.concurrent.Future
 
+/**
+  * Repository that stores [[ReservationRecord]].
+  */
 trait ReservationRecordRepository {
 
+  /**
+    * Stores the provided [[ReservationRecord]] in the repository.
+    * @param record
+    * @return
+    */
   def store(record: ReservationRecord): Future[Unit]
 
-  def find(podId: ReservationId): Future[Option[ReservationRecord]]
+  /**
+    * Retrieves the [[ReservationRecord]] if it exists.
+    * @param reservationId
+    * @return
+    */
+  def find(reservationId: ReservationId): Future[Option[ReservationRecord]]
 
 }
