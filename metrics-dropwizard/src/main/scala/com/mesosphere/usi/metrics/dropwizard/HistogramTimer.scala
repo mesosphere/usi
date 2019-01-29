fe84f75c991adc627af011c8a5100ba65543e0fe
Aleksey Dukhovniy <adukhovniy@mesosphere.io>
2019-01-29 11:26:08 +0100

    Add `metrics` and `metrics-dropwizard` modules to the project (#13)
    Summary:
    Ported Marathon's
    - `metrics` contains a common interface for metrics implementation. It defines a common `Metrics` interface as well as a set of interfaces for common metrics primitives like `Counter`, `Gauge` etc.
    - `metrics-dropwizard` contains Dropwizard metrics implementation
    - Additionally ported Marathon's metrics reporter: StatsD, DataDog (API and UDP) and Prometheus
    
    JIRA: DCOS-47507
    
    Co-authored-by: Ivan Chernetsky <ichernetsky@mesosphere.io>

---
+++ metrics-dropwizard/src/main/scala/com/mesosphere/usi/metrics/dropwizard/HistogramTimer.scala
@@ -0,0 +1,89 @@
+package mesosphere.marathon
+package metrics
+
+import akka.stream.scaladsl.{Flow, Source}
+import akka.stream.stage._
+import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
+import java.time.{Clock, Duration, Instant}
+
+import com.mesosphere.usi.async.ExecutionContexts
+import com.mesosphere.usi.metrics.{Timer, TimerAdapter}
+
+import scala.concurrent.Future
+import scala.util.control.NonFatal
+
+/**
+  * Akka Graph Stage that measures the time from the start of the stream until the end of it, where start is defined as
+  * the instant the stream is materialized
+  */
+class TimedStage[T](timer: TimerAdapter, clock: Clock) extends GraphStage[FlowShape[T, T]] {
+  val in = Inlet[T]("timer.in")
+  val out = Outlet[T]("timer.out")
+
+  @scala.throws[Exception](classOf[Exception])
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = {
+
+    val logic = new GraphStageLogic(shape) {
+      private val start: Instant = clock.instant
+
+      setHandler(in, new InHandler {
+        @scala.throws[Exception](classOf[Exception])
+        override def onPush(): Unit = push(out, grab(in))
+
+        @scala.throws[Exception](classOf[Exception])
+        override def onUpstreamFinish(): Unit = {
+          timer.update(Duration.between(start, clock.instant).toNanos)
+          super.onUpstreamFinish()
+        }
+
+        @scala.throws[Exception](classOf[Exception])
+        override def onUpstreamFailure(ex: Throwable): Unit = {
+          timer.update(Duration.between(start, clock.instant).toNanos)
+          super.onUpstreamFailure(ex)
+        }
+      })
+
+      setHandler(out, new OutHandler {
+        @scala.throws[Exception](classOf[Exception])
+        override def onPull(): Unit = pull(in)
+      })
+    }
+    logic
+  }
+
+  override def shape: FlowShape[T, T] = FlowShape.of(in, out)
+}
+
+case class HistogramTimer(timer: TimerAdapter) extends Timer {
+
+  override def apply[T](f: => Future[T]): Future[T] = {
+    val start = System.nanoTime()
+    val future = try {
+      f
+    } catch {
+      case NonFatal(e) =>
+        timer.update(System.nanoTime() - start)
+        throw e
+    }
+    future.onComplete(_ => timer.update(System.nanoTime() - start))(ExecutionContexts.callerThread)
+    future
+  }
+
+  override def forSource[T, M](f: => Source[T, M])(implicit clock: Clock = Clock.systemUTC): Source[T, M] = {
+    val src = f
+    val flow = Flow.fromGraph(new TimedStage[T](timer, clock))
+    val transformed = src.via(flow)
+    transformed
+  }
+
+  override def blocking[T](f: => T): T = {
+    val start = System.nanoTime()
+    try {
+      f
+    } finally {
+      timer.update(System.nanoTime() - start)
+    }
+  }
+
+  override def update(value: Long): Unit = timer.update(value)
+}
---
+++ metrics/README.md
@@ -0,0 +1,28 @@
+# Metrics
+
+This module defines a set of metrics interfaces. For a concrete implementation 
+see [metrics-dropwizard](http://github.com/mesosphere/usi/metrics-dropwizard) 
+module. 
+
+## Metric types
+
+USI has the following metric types:
+
+* a `counter` is a monotonically increasing integer, for instance, the
+  number of Mesos `revive` calls performed since the framework became
+  a leader.
+* a `gauge` is a current measurement, for instance, the number of tasks
+  currently known to the framework.
+* a `histogram` is a distribution of values in a stream of measurements,
+  for instance, the number of apps in group deployments.
+* a `meter` measures the rate at which a set of events occur.
+* a `timer` is a combination of a meter and a histogram, which measure
+  the duration of events and the rate of their occurrence.
+
+## Units of measurement
+
+A metric measures something either in abstract quantities, or in the
+following units:
+
+* `bytes`
+* `seconds`
