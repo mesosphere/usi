a0dc697c0b409d18537a209d7c71d474f320cda4
Nikita Melkozerov <nmelkozerov@mesosphere.io>
2019-01-29 16:42:11 +0100

    crud and comments

--- persistence/src/main/scala/com/mesosphere/usi/repository/PodRecordRepository.scala
+++ persistence/src/main/scala/com/mesosphere/usi/repository/PodRecordRepository.scala
@@ -5,22 +5,36 @@ import com.mesosphere.usi.core.models.{PodId, PodRecord}
 import scala.concurrent.Future
 
 /**
-  * Repository that stores [[PodRecord]].
+  * Repository for [[PodRecord]].
   */
 trait PodRecordRepository {
 
   /**
-    * Stores the provided [[PodRecord]] in the repository.
+    * Stores the provided [[PodRecord]] in the repository if it doesn't exist.
     * @param record
     * @return
     */
-  def store(record: PodRecord): Future[Unit]
+  def create(record: PodRecord): Future[PodId]
 
   /**
     * Retrieves the [[PodRecord]] if it exists.
     * @param podId
-    * @return
+    * @return Option(PodRecord) if it exists, None otherwise
     */
-  def find(podId: PodId): Future[Option[PodRecord]]
+  def read(podId: PodId): Future[Option[PodRecord]]
+
+  /**
+    * Updates the [[PodRecord]] if it exists.
+    * @param record
+    * @return id of the updated record
+    */
+  def update(record: PodRecord): Future[PodId]
+
+  /**
+    * Deletes the [[PodRecord]] if it exists.
+    * @param record
+    * @return id of the deleted record
+    */
+  def delete(record: PodRecord): Future[PodId]
 
 }
--- persistence/src/main/scala/com/mesosphere/usi/repository/ReservationRecordRepository.scala
+++ persistence/src/main/scala/com/mesosphere/usi/repository/ReservationRecordRepository.scala
@@ -5,22 +5,36 @@ import com.mesosphere.usi.core.models.{ReservationId, ReservationRecord}
 import scala.concurrent.Future
 
 /**
-  * Repository that stores [[ReservationRecord]].
+  * Repository for [[ReservationRecord]].
   */
 trait ReservationRecordRepository {
 
   /**
-    * Stores the provided [[ReservationRecord]] in the repository.
+    * Stores the provided [[ReservationRecord]] in the repository if it doesn't exist.
     * @param record
     * @return
     */
-  def store(record: ReservationRecord): Future[Unit]
+  def create(record: ReservationRecord): Future[ReservationId]
 
   /**
     * Retrieves the [[ReservationRecord]] if it exists.
-    * @param reservationId
-    * @return
+    * @param podId
+    * @return Option(ReservationRecord) if it exists, None otherwise
     */
-  def find(reservationId: ReservationId): Future[Option[ReservationRecord]]
+  def read(podId: ReservationId): Future[Option[ReservationRecord]]
+
+  /**
+    * Updates the [[ReservationRecord]] if it exists.
+    * @param record
+    * @return id of the updated record
+    */
+  def update(record: ReservationRecord): Future[ReservationId]
+
+  /**
+    * Deletes the [[ReservationRecord]] if it exists.
+    * @param record
+    * @return id of the deleted record
+    */
+  def delete(record: ReservationRecord): Future[ReservationId]
 
 }
