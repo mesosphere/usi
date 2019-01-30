8606e24db2cdf1e14063457652a66321d8fed297
Aleksey Dukhovniy <alex.dukhovniy@googlemail.com>
2019-01-30 17:09:05 +0100

    Added `test-utils` module
    Summary:
    - ported Marathon integration test classes. Most notably `MesosTest`, `ZookeeperServer` and `MesosFacade` and their dependencies
    - ported `MesosClientIntegrationTest` into the `mesos-client` project. It depends on the above mentioned test classes and starts Mesos/Zookeeper to run it's tests.
    
    JIRA: DCOS-47483

---
+++ test-utils/src/main/scala/com/mesosphere/utils/http/AkkaHttpResponse.scala
@@ -0,0 +1,37 @@
+package com.mesosphere.utils.http
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.stream.Materializer
+import play.api.libs.json.Reads
+
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.{ExecutionContext, Future}
+import scala.reflect.ClassTag
+
+object AkkaHttpResponse {
+
+  def request(request: HttpRequest)(implicit
+                                    actorSystem: ActorSystem,
+                                    materializer: Materializer,
+                                    ec: ExecutionContext,
+                                    timeout: FiniteDuration): Future[RestResult[HttpResponse]] = {
+    Http(actorSystem).singleRequest(request).flatMap { response =>
+      response.entity.toStrict(timeout).map(_.data.decodeString("utf-8")).map(RestResult(response, _))
+    }
+  }
+
+  def requestFor[T](httpRequest: HttpRequest)(implicit
+                                              actorSystem: ActorSystem,
+                                              materializer: Materializer,
+                                              ec: ExecutionContext,
+                                              timeout: FiniteDuration,
+                                              reads: Reads[T], classTag: ClassTag[T]): Future[RestResult[T]] = {
+    request(httpRequest).map(read[T])
+  }
+
+  def read[T](result: RestResult[HttpResponse])(implicit reads: Reads[T], ct: ClassTag[T]): RestResult[T] = {
+    RestResult(() => result.entityJson.as[T], result.originalResponse, result.entityString)
+  }
+}
