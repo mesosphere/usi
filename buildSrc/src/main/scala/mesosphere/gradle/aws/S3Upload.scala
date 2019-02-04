61db13b62b8ec31cef22d24c1644a35e80716f01
Karsten Jeschkies <k@jeschkies.xyz>
2019-02-04 17:57:50 +0100

    Archive JUnit XML reports.  (#31)
    Summary:
    This will upload the JUnit test reports alongside the HTML reports. The folder structure changes a little but on S3. The human readable reports will be in `<owner>/<repo>/<branch>/<build>/reports/tests` and the JUnit files in `<owner>/<repo>/<branch>/<build>/tests`.
    
    This patch will also migrate the S3 upload task to the AWS SDK 2.3.x and uses an asynchronous upload client.
    
    JIRA issue: DCOS-47520

--- buildSrc/src/main/scala/mesosphere/gradle/aws/S3Upload.scala
+++ buildSrc/src/main/scala/mesosphere/gradle/aws/S3Upload.scala
@@ -1,53 +1,91 @@
 package mesosphere.gradle.aws
 
-import java.io.File
+import java.util.concurrent.CompletableFuture
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
-import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
-import com.amazonaws.services.s3.transfer.Transfer.TransferState
-import com.amazonaws.services.s3.transfer.TransferManagerBuilder
+import org.apache.commons.io.FilenameUtils
 import com.typesafe.scalalogging.StrictLogging
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.FileTree
 import org.gradle.api.tasks.TaskAction
+import software.amazon.awssdk.core.async.AsyncRequestBody
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3AsyncClient
+import software.amazon.awssdk.services.s3.model.{PutObjectRequest, PutObjectResponse}
 
 import scala.beans.BeanProperty
 
+/**
+  * The S3Upload task defines a basic uploader for AWS S3.
+  */
 class S3Upload extends DefaultTask with StrictLogging {
 
-  val credentialProviderChain = new DefaultAWSCredentialsProviderChain()
+  lazy val s3: S3AsyncClient = S3AsyncClient.builder().region(Region.of(region)).build()
 
+  /**
+    * Defines the AWS region. Defaults to us-west-2.
+    */
   @BeanProperty var region: String = "us-west-2"
 
+  /**
+    * The bucket name for all uploaded objects.
+    */
   @BeanProperty var bucket: String = ""
 
+  /**
+    * The prefix used for all files to create the key. The S3 object key will be "$prefix/$filePath"
+    *
+    * Example:
+    *
+    * Given the prefix is "foo/bar"
+    * And we have a file "build/tests/failed.xml"
+    * And source points to "build/"
+    * Then the object key for "failed.xml" will be "foo/bar/tests/failed.xml"
+    */
   @BeanProperty var prefix: String = ""
 
-  @BeanProperty var source: File = null
+  /**
+    * Defines the root with filters for all files that should be uploaded to [[bucket]].
+    */
+  @BeanProperty var source: FileTree = null
 
   @TaskAction
   def run(): Unit = {
-    logger.info(s"Uploading files from $source to $bucket with prefix $prefix")
+    logger.info(s"Uploading files to $bucket with prefix $prefix")
 
-    val tx = TransferManagerBuilder.standard().withS3Client(createS3Client()).build()
-    val upload = tx.uploadDirectory(bucket, prefix, source, true)
-
-    while(!upload.isDone()) {
-      val progress = upload.getProgress()
-      logger.info(s"${progress.getPercentTransferred()} % ${upload.getState()}")
-      Thread.sleep(2000)
+    var pendingUploads = Vector.empty[CompletableFuture[PutObjectResponse]]
+    source.visit { fileDetails =>
+      if (!fileDetails.isDirectory) {
+        val file = fileDetails.getRelativePath()
+        val key = s"$prefix/$file"
+        val request = PutObjectRequest.builder()
+          .bucket(bucket)
+          .key(key)
+          .contentType(ContentType.of(fileDetails.getName))
+          .build()
+        val body = AsyncRequestBody.fromFile(fileDetails.getFile)
+        pendingUploads = pendingUploads :+ s3.putObject(request, body)
+      }
     }
 
-    tx.shutdownNow(true)
-    assert(upload.getState() == TransferState.Completed, s"Upload finished with ${upload.getState()}")
+    logger.info(s"Waiting for all ${pendingUploads.size} to finish.")
+    // Any exception is propagated.
+    pendingUploads.foreach(_.join())
 
     logger.info("Done.")
   }
 
-  /**
-    *  Returns AWS S3 client.
-    */
-  def createS3Client(): AmazonS3 = {
-    AmazonS3ClientBuilder.standard().withCredentials(credentialProviderChain).withRegion(region).build()
-  }
+}
 
+/**
+  * Defines a mapping from file extensions to MIME types, ie Content-type metadata.
+  */
+object ContentType {
+  val contentTypeMap = Map(
+    "html" -> "text/html",
+    "css" -> "text/css",
+    "js" -> "application/x-javascript",
+    "xml" -> "text/xml"
+  )
+
+  def of(fileName: String): String = contentTypeMap.getOrElse(FilenameUtils.getExtension(fileName), "text/plain")
 }
