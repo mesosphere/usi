c1cac0a3c74eb0e4eab4504b87941027894f8035
Tim Harper <tharper@mesosphere.com>
2019-02-06 01:08:47 -0700

    Docs

--- core/src/main/scala/com/mesosphere/usi/core/FrameEffects.scala
+++ core/src/main/scala/com/mesosphere/usi/core/FrameEffects.scala
@@ -3,6 +3,17 @@ package com.mesosphere.usi.core
 import com.mesosphere.usi.core.models.{PodId, PodRecord}
 import com.mesosphere.usi.models._
 
+/**
+  * Immutable effects builder
+  *
+  * Effects are appended in lazy fashion by using the exposed effect methods.
+  *
+  * Effects preserve order. For append-efficiency a link-list data structure is used. List is lazily reversed when
+  * effects are asked for.
+  *
+  * @param reverseStateEvents
+  * @param reverseMesosCalls
+  */
 case class FrameEffects(reverseStateEvents: List[StateEvent] = Nil, reverseMesosCalls: List[Mesos.Call] = Nil) {
   lazy val stateEvents = reverseStateEvents.reverse
   lazy val mesosCalls = reverseMesosCalls.reverse
@@ -16,13 +27,34 @@ case class FrameEffects(reverseStateEvents: List[StateEvent] = Nil, reverseMesos
       FrameEffects(other.reverseStateEvents ++ reverseStateEvents, other.reverseMesosCalls ++ reverseMesosCalls)
   }
 
+  /**
+    * Append an effect which will change some PodStatus.
+    *
+    * @param id PodId
+    * @param newStatus Optional value; None means remove
+    * @return A FrameEffects with this effect appended
+    */
   def withPodStatus(id: PodId, newStatus: Option[PodStatus]): FrameEffects =
     withStateUpdated(PodStatusUpdated(id, newStatus))
+
+  /**
+    * Append an effect which will change some PodRecord.
+    *
+    * @param id PodId
+    * @param newStatus Optional value; None means remove
+    * @return A FrameEffects with this effect appended
+    */
   def withPodRecord(id: PodId, newRecord: Option[PodRecord]): FrameEffects =
     withStateUpdated(PodRecordUpdated(id, newRecord))
 
   private def withStateUpdated(message: StateEvent): FrameEffects = copy(reverseStateEvents = message :: reverseStateEvents)
 
+  /**
+    * Append an effect which will cause some Mesos call to be emitted
+    *
+    * @param call The Mesos call
+    * @return A FrameEffects with this effect appended
+    */
   def withMesosCall(call: Mesos.Call): FrameEffects = copy(reverseMesosCalls = call :: reverseMesosCalls)
 }
 
