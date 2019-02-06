774cc8975cb9c25de96878fad1ed251a85d939e4
Aleksey Dukhovniy <alex.dukhovniy@googlemail.com>
2019-02-06 11:35:25 +0100

    Update only Mesosphere repository with `apt-get update`

--- ci/provision.sh
+++ ci/provision.sh
@@ -3,12 +3,17 @@ set -x -e -o pipefail
 
 MESOS_VERSION=$1
 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 CODENAME=$(lsb_release -cs)
 
+# Add Mesosphere repo to the list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E56151BF
 echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | \
     sudo tee /etc/apt/sources.list.d/mesosphere.list
-apt-get -y update
 
+# Update only Mesosphere repository
+apt-get -y update -o Dir::Etc::sourcelist="sources.list.d/mesosphere.list" \
+    -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+
+# Install Mesos
 apt-get -y install mesos="$MESOS_VERSION-2.0.3" zookeeperd
