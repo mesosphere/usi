67ba292b4c26c9d591e37a720c0049e7587ad1ce
Aleksey Dukhovniy <adukhovniy@mesosphere.io>
2019-02-06 13:06:27 +0100

    Add missing keys for the `apt-get update` (#35)
    Summary:
    from time to time a repository key is missing so we add it manually. Additionally, we now exit the build if provisioning failed.

--- ci/provision.sh
+++ ci/provision.sh
@@ -3,12 +3,17 @@ set -x -e -o pipefail
 
 MESOS_VERSION=$1
 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 CODENAME=$(lsb_release -cs)
 
+# Add Mesosphere repo to the list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E56151BF
 echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | \
     sudo tee /etc/apt/sources.list.d/mesosphere.list
+
+# Some keys are missing from time to time - add them manually
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6B05F25D762E3157
 apt-get -y update
 
-apt-get -y install mesos="$MESOS_VERSION-2.0.3" zookeeperd
+# Install Mesos
+apt-get -y install mesos="$MESOS_VERSION-2.0.3"
