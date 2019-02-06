92e759c5617ebf2898396078a6461ca127bd8fbd
Aleksey Dukhovniy <adukhovniy@mesosphere.io>
2019-02-06 01:21:14 -0700

    Update core-models/src/main/scala/com/mesosphere/usi/core/models/RunSpec.scala
    Co-Authored-By: timcharper <tharper@mesosphere.io>

--- core-models/src/main/scala/com/mesosphere/usi/core/models/RunSpec.scala
+++ core-models/src/main/scala/com/mesosphere/usi/core/models/RunSpec.scala
@@ -4,4 +4,4 @@ package com.mesosphere.usi.core.models
   * Specification used to launch a [[PodSpec]]
   * @param cpus number of cpus requested by the pod
   */
-case class RunSpec(cpus: Int)
+case class RunSpec(cpus: Double)
