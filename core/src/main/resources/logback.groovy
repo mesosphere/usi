dbe8aebd79aa63f37e3ff81b7d6f368e40598460
Tarun Gupta Akirala <tarugupta.92@gmail.com>
2019-02-07 15:21:51 -0500

    Add a tcp logger

--- core/src/main/resources/logback.groovy
+++ core/src/main/resources/logback.groovy
@@ -1,48 +1,105 @@
 import net.logstash.logback.appender.LoggingEventAsyncDisruptorAppender
+import net.logstash.logback.appender.LogstashTcpSocketAppender
 import net.logstash.logback.composite.LogstashVersionJsonProvider
 import net.logstash.logback.composite.loggingevent.*
 import net.logstash.logback.encoder.*
+import net.logstash.logback.fieldnames.LogstashFieldNames
 import net.logstash.logback.stacktrace.ShortenedThrowableConverter
 
-appender("CONSOLE_JSON", ConsoleAppender) {
+def appenderList = ["ASYNC"]
+def env = System.getenv()
+def USI_TCP_DESTINATION = env.get("USI_TCP_LOG_DESTINATION")
+
+static def defaultJsonProvider() {
+    def json = new LoggingEventJsonProviders()
+    json.addTimestamp(tsProvider())
+    json.addThreadName(threadNameProvider())
+    json.addLogLevel(new LogLevelJsonProvider())
+    json.addLoggerName(loggerNameProvider())
+    json.addMessage(msgProvider())
+    json.addCallerData(callerDataProvider())
+    json.addStackTrace(stackTraceProvider())
+    json.addMdc(new MdcJsonProvider())
+    json.addArguments(new ArgumentsJsonProvider())
+    json.addVersion(versionProvider())
+    return json
+}
+
+static def tsProvider() {
+    def ts = new LoggingEventFormattedTimestampJsonProvider()
+    ts.setFieldName('ts')
+    ts.setPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
+    return ts
+}
+
+static def threadNameProvider() {
+    def threadName = new ThreadNameJsonProvider()
+    threadName.setFieldName("thread")
+    return threadName
+}
+
+static def loggerNameProvider() {
+    def loggerName = new LoggerNameJsonProvider()
+    loggerName.setFieldName("logger")
+    loggerName.setShortenedLoggerNameLength(35)
+    return loggerName
+}
+
+static def msgProvider() {
+    def msg = new MessageJsonProvider()
+    msg.setFieldName("msg")
+    return msg
+}
+
+static def callerDataProvider() {
+    def callerData = new CallerDataJsonProvider()
+    LogstashFieldNames f = new LogstashFieldNames()
+    f.setCallerClass("[ignore]")
+    callerData.setFieldNames(f)
+    return callerData
+}
+
+static def stackTraceProvider() {
+    def stackTrace = new StackTraceJsonProvider()
+    def throwableConverter = new ShortenedThrowableConverter()
+    throwableConverter.setExcludes(Arrays.asList(
+        /sun\..*/,
+        /groovy\..*/,
+        /com\.sun\..*/
+    ))
+    throwableConverter.setShortenedClassNameLength(35)
+    throwableConverter.setRootCauseFirst(true)
+    stackTrace.setThrowableConverter(throwableConverter)
+    stackTrace
+}
+
+static def versionProvider() {
+    def ver = new LogstashVersionJsonProvider()
+    ver.setWriteAsInteger(true)
+    return ver
+}
+
+appender("CONSOLE", ConsoleAppender) {
     encoder(LoggingEventCompositeJsonEncoder) {
-        providers(LoggingEventJsonProviders) {
-            timestamp(LoggingEventFormattedTimestampJsonProvider) {
-                fieldName = '@ts'
-                pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS"
-            }
-            threadName(ThreadNameJsonProvider) {
-                fieldName = 'thread'
-            }
-            logLevel(LogLevelJsonProvider)
-            loggerName(LoggerNameJsonProvider) {
-                fieldName = 'logger'
-                shortenedLoggerNameLength = 35
-            }
-            message(MessageJsonProvider) {
-                fieldName = 'msg'
-            }
-            arguments(ArgumentsJsonProvider)
-            mdc(MdcJsonProvider)
-            stackTrace(StackTraceJsonProvider) {
-                throwableConverter(ShortenedThrowableConverter) {
-                    shortenedClassNameLength = 40
-                    exclude = /sun\..*/
-                    exclude = /groovy\..*/
-                    exclude = /com\.sun\..*/
-                    rootCauseFirst = true
-                }
-            }
-            version(LogstashVersionJsonProvider) {
-                writeAsInteger = true
-            }
-        }
+        providers = defaultJsonProvider()
     }
 }
 
+if (USI_TCP_DESTINATION != null && !USI_TCP_DESTINATION.isEmpty()) {
+    println "Posting logs over tcp to : $USI_TCP_DESTINATION"
+    appender("TCP", LogstashTcpSocketAppender) {
+        destination = USI_TCP_DESTINATION
+        encoder(LoggingEventCompositeJsonEncoder) {
+            providers = defaultJsonProvider()
+        }
+    }
+    appenderList.add("TCP")
+}
+
 appender("ASYNC", LoggingEventAsyncDisruptorAppender) {
-    appenderRef("CONSOLE_JSON")
+    appenderRef("CONSOLE")
+    includeCallerData = true
     ringBufferSize = 8192
 }
 
-root(INFO, ["ASYNC"])
+root(TRACE, appenderList)
