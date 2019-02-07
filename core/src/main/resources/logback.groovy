6f856a0eb33baed5084a263f20e9cb603f558207
Tarun Gupta Akirala <tarugupta.92@gmail.com>
2019-02-07 11:03:00 -0500

    Use LoggingEventCompositeJsonEncoder

--- core/src/main/resources/logback.groovy
+++ core/src/main/resources/logback.groovy
@@ -1,18 +1,47 @@
-import net.logstash.logback.encoder.LogstashEncoder
 import net.logstash.logback.appender.LoggingEventAsyncDisruptorAppender
-import net.logstash.logback.fieldnames.LogstashFieldNames
+import net.logstash.logback.composite.LogstashVersionJsonProvider
+import net.logstash.logback.composite.loggingevent.*
+import net.logstash.logback.encoder.*
+import net.logstash.logback.stacktrace.ShortenedThrowableConverter
 
-appender("CONSOLE", ConsoleAppender) {
-    encoder(LogstashEncoder){
-        LogstashFieldNames l = new LogstashFieldNames()
-        l.setVersion("[ignore]")
-        l.setLevelValue("[ignore]")
-        fieldNames = l
+appender("CONSOLE_JSON", ConsoleAppender) {
+    encoder(LoggingEventCompositeJsonEncoder) {
+        providers(LoggingEventJsonProviders) {
+            timestamp(LoggingEventFormattedTimestampJsonProvider) {
+                fieldName = '@ts'
+                pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+            }
+            threadName(ThreadNameJsonProvider) {
+                fieldName = 'thread'
+            }
+            logLevel(LogLevelJsonProvider)
+            loggerName(LoggerNameJsonProvider) {
+                fieldName = 'logger'
+                shortenedLoggerNameLength = 35
+            }
+            message(MessageJsonProvider) {
+                fieldName = 'msg'
+            }
+            arguments(ArgumentsJsonProvider)
+            mdc(MdcJsonProvider)
+            stackTrace(StackTraceJsonProvider) {
+                throwableConverter(ShortenedThrowableConverter) {
+                    shortenedClassNameLength = 40
+                    exclude = /sun\..*/
+                    exclude = /groovy\..*/
+                    exclude = /com\.sun\..*/
+                    rootCauseFirst = true
+                }
+            }
+            version(LogstashVersionJsonProvider) {
+                writeAsInteger = true
+            }
+        }
     }
 }
 
 appender("ASYNC", LoggingEventAsyncDisruptorAppender) {
-    appenderRef("CONSOLE")
+    appenderRef("CONSOLE_JSON")
     ringBufferSize = 8192
 }
 
