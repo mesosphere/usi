ae831ed349caba7f36a30528c31dbf36ea6c918c
Tarun Gupta Akirala <tarugupta.92@gmail.com>
2019-02-13 16:09:22 -0500

    revert unintentional changes in hello-world module

--- examples/hello-world/src/main/scala/com/mesosphere/usi/helloworld/Main.scala
+++ examples/hello-world/src/main/scala/com/mesosphere/usi/helloworld/Main.scala
@@ -1,15 +1,6 @@
 package com.mesosphere.usi.helloworld
 
-import com.mesosphere.usi.core.Scheduler
-import com.mesosphere._
-import com.typesafe.scalalogging.Logger
-
 /**
   * A demonstration of USI features.
   */
-object Main extends App {
-  val composedLogger = Logger.takingImplicit[KvArgs](getClass).withCtx("hello", "world")
-  val moreCtx = composedLogger.ctx.and("hostName", "localhost")
-  composedLogger.info("initiating connection")(moreCtx)
-  Scheduler.connect("asdasd")
-}
+object Main extends App {}
