6bcb9bea182687b776052c6fbaafa3003101e0b4
Tim Harper <tharper@mesosphere.com>
2019-02-14 02:19:14 -0700

    doc ResourceRequirement

--- core-models/src/main/scala/com/mesosphere/usi/core/models/ResourceRequirement.scala
+++ core-models/src/main/scala/com/mesosphere/usi/core/models/ResourceRequirement.scala
@@ -2,8 +2,26 @@ package com.mesosphere.usi.core.models
 
 import org.apache.mesos.v1.{Protos => Mesos}
 
+/**
+  * Used for resource matching.
+  */
 trait ResourceRequirement {
+  /**
+    * A human readable description for this resource requirement
+    */
   def description: String
+
+  /**
+    * The ResourceType this requirement describes
+    */
   def resourceType: ResourceType
-  def matchAndConsume(resource: Seq[Mesos.Resource]): Option[ResourceMatchResult]
+
+  /**
+    * Logic for matching and consuming resources. Method considers the provided resources, and then matches and consumes
+    * the applicable resources
+    *
+    * @param resources for matching. The Scheduler will only provide the resources matching the [[ResourceType]] for this [[ResourceRequirement]].
+    * @return Some match result if it matches, None if not.
+    */
+  def matchAndConsume(resources: Seq[Mesos.Resource]): Option[ResourceMatchResult]
 }
