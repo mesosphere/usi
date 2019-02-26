a3b14f063c71f7fb700d7c39cd30da599a529ecf
Kaiwalya Joshi <kjoshi@mesosphere.com>
2019-02-26 01:52:47 -0800

    Introduce a new `persistence-zookeeper` module (#42)
    This series of patches introduces a new `persistence-zookeeper` module and transplants the following code from Marathon: 
    - low-level ZK storage implementation aka `ZooKeeperPersistanceStore` and its helper classes
    - a few additional testing classes like `DummyMetrics` and `Mockito`
    
    Keep in mind: this is only the **low-level storage** classes. The concrete repository implementation (of e.g. `PodRecordRepository` and others) will follow in a separate PR.
    
    JIRA Issues: DCOS-48210
    
    Co-authored-by: Aleksey Dukhovniy <alex.dukhovniy@googlemail.com>
    Co-authored-by: Karsten Jeschkies <k@jeschkies.xyz>

---
+++ persistence-zookeeper/src/main/scala/com/mesosphere/usi/storage/zookeeper/PersistenceStore.scala
@@ -0,0 +1,115 @@
+package com.mesosphere.usi.storage.zookeeper
+
+import akka.stream.scaladsl.{Flow, Source}
+import akka.util.ByteString
+import akka.{Done, NotUsed}
+import com.mesosphere.usi.storage.zookeeper.PersistenceStore.{Node, StoreOp}
+
+import scala.collection.immutable.Seq
+import scala.concurrent.Future
+import scala.util.Try
+
+/**
+  * An interface for a simple persistence store abstraction for the underlying Zookeeper store. Supported are basic
+  * create, read, update, delete and check methods along with transaction operations and Zookeeper specific children
+  * and sync methods.
+  */
+trait PersistenceStore {
+
+  /**
+    * A Flow for saving nodes to the store. It takes a stream of nodes and returns a stream of node keys
+    * that were successfully stored.
+    */
+  def createFlow: Flow[Node, String, NotUsed]
+  def create(node: Node): Future[String]
+  def create(nodes: Seq[Node]): Source[String, NotUsed] = Source(nodes).via(createFlow)
+
+  /**
+    * A Flow for reading nodes from the store. It takes a stream of node paths and returns a stream of Try[Node] elements.
+    */
+  def readFlow: Flow[String, Try[Node], NotUsed]
+  def read(path: String): Future[Try[Node]]
+  def read(paths: Seq[String]): Source[Try[Node], NotUsed] = Source(paths).via(readFlow)
+
+  /**
+    * A Flow for updating nodes in the store. It takes a stream of nodes and returns a stream of paths to indicate
+    * a successful update operation for the returned path.
+    */
+  def updateFlow: Flow[Node, String, NotUsed]
+  def update(node: Node): Future[String]
+  def update(nodes: Seq[Node]): Source[String, NotUsed] = Source(nodes).via(updateFlow)
+
+  /**
+    * A Flow for deleting nodes from the repository. It takes a stream of paths and returns a stream of paths to indicate
+    * a successful deletion operation for the returned path.
+    */
+  def deleteFlow: Flow[String, String, NotUsed]
+  def delete(path: String): Future[String]
+  def delete(paths: Seq[String]): Source[String, NotUsed] = Source(paths).via(deleteFlow)
+
+  /**
+    * Returns the list of paths for children nodes for the passed path. Note that returned path is absolute and contains
+    * node's path prefix.
+    *
+    * @return
+    */
+  type Children = Seq[String]
+  def childrenFlow(absolute: Boolean): Flow[String, Try[Children], NotUsed]
+  def children(path: String, absolute: Boolean): Future[Try[Children]]
+  def children(paths: Seq[String], absolute: Boolean): Source[Try[Children], NotUsed] =
+    Source(paths).via(childrenFlow(absolute))
+
+  /**
+    * Checks for the existence of a node with passed path. The [[existsFlow]] returns a [[scala.Tuple2]] with a path
+    * and a boolean for it's existence.
+    *
+    * @return
+    */
+  def existsFlow: Flow[String, (String, Boolean), NotUsed]
+  def exists(path: String): Future[Boolean]
+  def exists(paths: Seq[String]): Source[(String, Boolean), NotUsed] = Source(paths).via(existsFlow)
+
+  /**
+    * Method syncs state with underlying store.
+    *
+    * @param path node path to sync
+    * @return
+    */
+  def sync(path: String): Future[Done]
+
+  /**
+    * Method takes a list of transaction [[com.mesosphere.usi.storage.zookeeper.PersistenceStore.StoreOp]] operations
+    * and submits them. An exception is thrown if one of the operations fail.
+    */
+  def transaction(operations: Seq[StoreOp]): Future[Done]
+
+  /**
+    * Create a node if none exists with the given path. Return the path of the node. This operation is atomic.
+    *
+    * @param node to create
+    * @return
+    */
+  def createIfAbsent(node: Node): Future[String]
+}
+
+object PersistenceStore {
+
+  /**
+    * Main storage unit: it's a key-value like structure, where the key is the path
+    * to the node e.g. `/app1` which can also be nested e.g. `/apps/app1` and the data
+    * is the value (represented as ByteString)
+    *
+    * @param path node's path
+    * @param data node's payload
+    */
+  case class Node(path: String, data: ByteString)
+
+  /**
+    * Helper wrappers around transaction operations.
+    */
+  sealed trait StoreOp
+  case class CreateOp(node: Node) extends StoreOp
+  case class UpdateOp(node: Node) extends StoreOp
+  case class DeleteOp(path: String) extends StoreOp
+  case class CheckOp(path: String) extends StoreOp
+}
---
+++ test-utils/src/main/scala/com/mesosphere/utils/metrics/DummyMetrics.scala
@@ -0,0 +1,52 @@
+package com.mesosphere.utils.metrics
+
+import java.time.Clock
+
+import akka.stream.scaladsl.Source
+import com.mesosphere.usi.metrics.{ClosureGauge, Counter, Gauge, Meter, Metrics, SettableGauge, Timer}
+import com.mesosphere.usi.metrics.{UnitOfMeasurement => DropwizardUnitOfMeasurement}
+
+import scala.concurrent.Future
+
+/**
+  * As the name suggest, this is a dummy metrics implementation of the [[Metrics]] interface found in the `metrics`
+  * module. Should be used for testing only.
+  */
+object DummyMetrics extends Metrics {
+  class DummyCounter extends Counter {
+    override def increment(): Unit = ()
+    override def increment(times: Long): Unit = ()
+  }
+  class DummyGauge extends SettableGauge {
+    override def increment(by: Long): Unit = ()
+    override def decrement(by: Long): Unit = ()
+    override def value(): Long = 0L
+    override def setValue(value: Long): Unit = ()
+  }
+  class DummyClosureGauge extends ClosureGauge
+  class DummyMeter extends Meter {
+    override def mark(): Unit = ()
+  }
+  class DummyTimer extends Timer {
+    override def apply[T](f: => Future[T]): Future[T] = f
+    override def blocking[T](f: => T): T = f
+    override def forSource[T, M](f: => Source[T, M])(implicit clock: Clock): Source[T, M] = f
+    override def update(value: Long): Unit = ()
+  }
+
+  override def counter(name: String, unit: DropwizardUnitOfMeasurement = DropwizardUnitOfMeasurement.None): Counter =
+    new DummyCounter
+  override def gauge(name: String, unit: DropwizardUnitOfMeasurement = DropwizardUnitOfMeasurement.None): Gauge =
+    new DummyGauge
+  override def closureGauge[N](
+      name: String,
+      currentValue: () => N,
+      unit: DropwizardUnitOfMeasurement = DropwizardUnitOfMeasurement.None): ClosureGauge = new DummyClosureGauge
+  override def settableGauge(
+      name: String,
+      unit: DropwizardUnitOfMeasurement = DropwizardUnitOfMeasurement.None): SettableGauge =
+    new DummyGauge
+
+  override def meter(name: String): Meter = new DummyMeter
+  override def timer(name: String): Timer = new DummyTimer
+}
