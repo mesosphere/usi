69d658e96fa2a700b4451406116e27711c22aad8
Alena Varkockova <varkockova.a@gmail.com>
2019-03-07 10:59:54 +0100

    Add ranged/ports resource matching (#56)
    This reuses a lot of code from Marathon.
    
    It implements following two strategies:
    
    - Match exactly these port numbers
    - Take any random X ports
    - Take N consecutive ports (stretch goal) will be implemented in another PR.
    
    It does not work with reservations and roles right now.
    
    JIRA: DCOS-47481

--- core/src/test/scala/com/mesosphere/usi/core/integration/SchedulerIntegrationTest.scala
+++ core/src/test/scala/com/mesosphere/usi/core/integration/SchedulerIntegrationTest.scala
@@ -6,7 +6,7 @@ import com.mesosphere.mesos.client.MesosClient
 import com.mesosphere.mesos.conf.MesosClientSettings
 import com.mesosphere.usi.core.Scheduler
 import com.mesosphere.usi.core.helpers.SchedulerStreamTestHelpers.{outputFlatteningSink, specInputSource}
-import com.mesosphere.usi.core.matching.ScalarResource
+import com.mesosphere.usi.core.matching.{RangeResource, ScalarResource}
 import com.mesosphere.usi.core.models._
 import com.mesosphere.utils.AkkaUnitTest
 import com.mesosphere.utils.mesos.MesosClusterTest
@@ -15,26 +15,30 @@ import org.apache.mesos.v1.Protos.FrameworkInfo
 import org.scalatest.Inside
 
 class SchedulerIntegrationTest extends AkkaUnitTest with MesosClusterTest with Inside {
+  implicit val materializer = ActorMaterializer()
+
+  lazy val settings = MesosClientSettings(mesosFacade.url)
+  val frameworkInfo = Protos.FrameworkInfo
+    .newBuilder()
+    .setUser("test")
+    .setName("SimpleHelloWorldExample")
+    .addRoles("test")
+    .addCapabilities(FrameworkInfo.Capability.newBuilder().setType(FrameworkInfo.Capability.Type.MULTI_ROLE))
+    .build()
+
+  lazy val mesosClient: MesosClient = MesosClient(settings, frameworkInfo).runWith(Sink.head).futureValue
+  lazy val schedulerFlow = Scheduler.fromClient(mesosClient)
+  lazy val (input, output) = specInputSource(SpecsSnapshot.empty)
+    .via(schedulerFlow)
+    .toMat(outputFlatteningSink)(Keep.both)
+    .run
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+  }
+
   "launches the thing" in {
-    implicit val materializer = ActorMaterializer()
-
-    val settings = MesosClientSettings(mesosFacade.url)
-    val frameworkInfo = Protos.FrameworkInfo
-      .newBuilder()
-      .setUser("test")
-      .setName("SimpleHelloWorldExample")
-      .addRoles("test")
-      .addCapabilities(FrameworkInfo.Capability.newBuilder().setType(FrameworkInfo.Capability.Type.MULTI_ROLE))
-      .build()
-
-    val client = MesosClient(settings, frameworkInfo).runWith(Sink.head).futureValue
-    val schedulerFlow = Scheduler.fromClient(client)
-
     val podId = PodId("scheduler-integration-test-pod")
-    val (input, output) = specInputSource(SpecsSnapshot.empty)
-      .via(schedulerFlow)
-      .toMat(outputFlatteningSink)(Keep.both)
-      .run
 
     input.offer(
       PodSpecUpdated(
@@ -58,6 +62,31 @@ class SchedulerIntegrationTest extends AkkaUnitTest with MesosClusterTest with I
         podRecord.id shouldBe podId
         println(s"agentId = ${podRecord.newRecord.get.agentId}")
     }
+    eventually {
+      inside(output.pull().futureValue) {
+        case Some(podStatusChange: PodStatusUpdated) =>
+          podStatusChange.newStatus.get
+            .taskStatuses(TaskId(podId.value))
+            .getState shouldBe Protos.TaskState.TASK_RUNNING
+      }
+    }
+  }
+
+  "launches pod with ports" in {
+    val podId = PodId("pod-with-ports")
+
+    input.offer(
+      PodSpecUpdated(
+        podId,
+        Some(PodSpec(
+          podId,
+          Goal.Running,
+          RunSpec(
+            resourceRequirements = List(ScalarResource.cpus(1), ScalarResource.memory(256), RangeResource.ports(Seq(0))),
+            shellCommand = "sleep 3600")
+        ))
+      ))
+
     eventually {
       inside(output.pull().futureValue) {
         case Some(podStatusChange: PodStatusUpdated) =>
