f0161a70591bca6fea2d354f8a8b323b55e274d8
Alena Varkockova <varkockova.a@gmail.com>
2019-03-13 15:14:19 +0100

    Formatting

--- core/src/test/scala/com/mesosphere/usi/core/integration/SchedulerIntegrationTest.scala
+++ core/src/test/scala/com/mesosphere/usi/core/integration/SchedulerIntegrationTest.scala
@@ -86,7 +86,8 @@ class SchedulerIntegrationTest extends AkkaUnitTest with MesosClusterTest with I
             resourceRequirements =
               List(ScalarRequirement.cpus(1), ScalarRequirement.memory(256), RangeRequirement.ports(Seq(0))),
             shellCommand = "sleep 3600",
-            Seq("test"))
+            Seq("test")
+          )
         ))
       ))
 
