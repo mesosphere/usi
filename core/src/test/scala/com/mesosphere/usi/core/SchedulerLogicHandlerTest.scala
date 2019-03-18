ec59099f98f45edae308de6a98d445a336a0fdc1
Alena Varkockova <varkockova.a@gmail.com>
2019-03-18 17:04:27 +0100

    Add suppress test

--- core/src/test/scala/com/mesosphere/usi/core/SchedulerLogicHandlerTest.scala
+++ core/src/test/scala/com/mesosphere/usi/core/SchedulerLogicHandlerTest.scala
@@ -3,16 +3,19 @@ package com.mesosphere.usi.core
 import com.mesosphere.mesos.client.MesosCalls
 import com.mesosphere.usi.core.helpers.MesosMock
 import com.mesosphere.usi.core.models.{Goal, PodId, PodSpec, PodSpecUpdated, RunSpec}
+import com.mesosphere.usi.core.protos.ProtoBuilders
 import com.mesosphere.utils.UnitTest
 
 class SchedulerLogicHandlerTest extends UnitTest {
-  "output a mesos call with revive for new podspec role" in {
+  "produce a mesos call with revive for new podspec role" in {
     Given("Scheduler logic handler with empty state")
     val handler = new SchedulerLogicHandler(new MesosCalls(MesosMock.mockFrameworkId))
     val podId = PodId("pod")
+
     When("pod with role 'test-role' is updated")
     val result = handler.handleSpecEvent(
       PodSpecUpdated(podId, Some(PodSpec(podId, Goal.Running, RunSpec(Seq.empty, "", "test-role")))))
+
     Then("revive call is generated for that role")
     result.mesosCalls.exists(c => c.hasRevive && c.getRevive.getRoles(0) == "test-role") should be(true) withClue s"Expecting revive call with role 'test-role' but got ${result.mesosCalls}"
 
@@ -21,6 +24,22 @@ class SchedulerLogicHandlerTest extends UnitTest {
     val result2 = handler.handleSpecEvent(
       PodSpecUpdated(podId2, Some(PodSpec(podId2, Goal.Running, RunSpec(Seq.empty, "", "test-role")))))
     result2.mesosCalls.exists(c => c.hasRevive && c.getRevive.getRoles(0) == "test-role") should be(true) withClue s"Expecting revive call with role 'test-role' but got ${result.mesosCalls}"
+  }
+
+  "produce a mesos call with suppress when all podspecs for that roles were launched" in {
+    Given("Scheduler logic handler with not launched pod")
+    val handler = new SchedulerLogicHandler(new MesosCalls(MesosMock.mockFrameworkId))
+    val podId = PodId("pod")
+    // creates not launched pod in the internal state of scheduler logic
+    handler.handleSpecEvent(
+      PodSpecUpdated(podId, Some(PodSpec(podId, Goal.Running, RunSpec(Seq.empty, "", "test-role")))))
+
+    When("that pod is launched")
+    val offer = MesosMock.createMockOffer()
+    val result = handler.handleMesosEvent(ProtoBuilders.newOfferEvent(offer))
+
+    Then("suppress call is generated for that role")
+    result.mesosCalls.exists(c => c.hasSuppress && c.getSuppress.getRoles(0) == "test-role") should be(true) withClue s"Expecting suppress call with role 'test-role' but got ${result.mesosCalls}"
 
   }
 }
