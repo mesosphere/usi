b45f73574eff5362963e47dbe23f8601d2090f02
Nikita Melkozerov <nmelkozerov@mesosphere.com>
2019-04-16 16:25:43 +0200

    Rebase on master

---
+++ examples/keep-alive-framework/README.md
@@ -0,0 +1,6 @@
+# Keep-Alive Example Framework
+
+Run the keep-alive example framework that:
+- uses simplified Scheduler interface
+- starts 100 `echo "Hello, world" && sleep 5` tasks
+- keeps restarting the task if it finishes
---
+++ examples/keep-alive-framework/src/main/scala/com/mesosphere/usi/helloworld/KeepAliveMesosClientFactory.scala
@@ -0,0 +1,31 @@
+package com.mesosphere.usi.helloworld
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Sink
+import com.mesosphere.mesos.client.MesosClient
+import com.mesosphere.mesos.conf.MesosClientSettings
+import com.typesafe.config.Config
+import org.apache.mesos.v1.Protos.FrameworkInfo
+
+import scala.concurrent.duration._
+import scala.concurrent.Await
+import scala.sys.SystemProperties
+
+class KeepAliveMesosClientFactory(conf: Config)(implicit system: ActorSystem, mat: ActorMaterializer) {
+
+  val settings = MesosClientSettings(conf.getString("master-url"))
+  val frameworkInfo = FrameworkInfo
+    .newBuilder()
+    .setUser(
+      new SystemProperties()
+        .get("user.name")
+        .getOrElse(throw new IllegalArgumentException("A local user is needed to launch Mesos tasks")))
+    .setName("KeepAliveFramework")
+    .addRoles("test")
+    .addCapabilities(FrameworkInfo.Capability.newBuilder().setType(FrameworkInfo.Capability.Type.MULTI_ROLE))
+    .setFailoverTimeout(0d)
+    .build()
+
+  val client: MesosClient = Await.result(MesosClient(settings, frameworkInfo).runWith(Sink.head), 10.seconds)
+}
