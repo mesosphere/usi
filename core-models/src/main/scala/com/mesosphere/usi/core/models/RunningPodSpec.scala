a5381accfbe3da04c221cd1564ca2ac02381021b
Tim Harper <tharper@mesosphere.com>
2019-05-10 12:37:02 +0200

    docs

--- core-models/src/main/scala/com/mesosphere/usi/core/models/RunningPodSpec.scala
+++ core-models/src/main/scala/com/mesosphere/usi/core/models/RunningPodSpec.scala
@@ -3,21 +3,26 @@ package com.mesosphere.usi.core.models
 import com.mesosphere.usi.core.models.resources.{ExactValue, RangeRequirement, ResourceRequirement, ScalarRequirement}
 
 /**
-  * Scheduler logic state indicating the action to take for some pod [[RunningPodSpec]] or [[TerminalPodSpec]]
+  * Scheduler logic state indicating that some action still needs to be done some pod [[RunningPodSpec]] or
+  * [[TerminalPodSpec]]
   */
 sealed trait PodSpec {
   val id: PodId
   def shouldBeTerminal: Boolean
 }
 
+/**
+  * Used by the scheduler to track that a pod needs to be killed
+  * @param id
+  */
 case class TerminalPodSpec(id: PodId) extends PodSpec {
   override def shouldBeTerminal: Boolean = true
 }
 
 /**
-  * Scheduler logic state indicating that a pod should be launched (and isn't)
+  * Used by the scheduler to track that a pod pod should be launched (and, isn't yet.)
   *
-  * Pods are launched at-most-once. It is illegal to transition of [[RunningPodSpec]] from goal terminal to goal running.
+  * Pods are launched at-most-once.
   *
   * @param id Id of the pod
   * @param runSpec WIP the thing to run, and resource requirements, etc.
