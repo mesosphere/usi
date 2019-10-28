1f99df6c96f5499a857c5a29e5b410f83699e05d
Karsten Jeschkies <k@jeschkies.xyz>
2019-10-28 15:59:17 +0100

    Consume same Mesos master addresses as libprocess.
    Summary:
    The Mesos scheduler consumes Mesos master addresses in the following
    formats:
       * host:port
       * zk://host1:port1,host2:port2,.../path
       * zk://username:password@host1:port1,host2:port2,.../path
    
    In case it's a `zk://` address we are fetching the Mesos master
    information from the provided path. It follows the parsing and master
    identification as described in the [High Availability](http://mesos.apache.org/documentation/latest/high-availability/#implementation-details)
    documentation and implemented in [detector/zookeeper](https://github.com/apache/mesos/blob/master/src/master/detector/zookeeper.cpp).
    
    JIRA issues: DCOS_OSS-5588

---
+++ mesos-client/src/main/scala/com/mesosphere/mesos/client/MasterDetector.scala
@@ -0,0 +1,114 @@
+package com.mesosphere.mesos.client
+import java.net.URL
+import java.util.concurrent.CompletionStage
+
+import com.typesafe.scalalogging.StrictLogging
+import org.apache.curator.framework.CuratorFrameworkFactory
+import org.apache.curator.retry.RetryOneTime
+import org.apache.curator.x.async.AsyncCuratorFramework
+import org.apache.mesos.v1.Protos
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+
+import scala.async.Async.{async, await}
+import scala.collection.JavaConverters._
+import scala.compat.java8.FutureConverters._
+import scala.concurrent.{ExecutionContext, Future}
+
+trait MasterDetector {
+
+  /** @return the Mesos master URL for the cluster. */
+  def getMaster()(implicit ex: ExecutionContext): CompletionStage[URL]
+}
+
+object MasterDetector {
+
+  /**
+    * Constructs a master detector base on the master string passed.
+    *
+    * @param master The master string should be one of:
+    *               host:port
+    *               http://host:port
+    *               zk://host1:port1,host2:port2,.../path
+    *               zk://username:password@host1:port1,host2:port2,.../path
+    * @return A master detector.
+    */
+  def apply(master: String): MasterDetector = {
+    if (master.startsWith("zk://")) {
+      Zookeeper(master)
+    } else {
+      Standalone(master)
+    }
+  }
+}
+
+case class Zookeeper(master: String) extends MasterDetector with StrictLogging {
+  require(master.startsWith("zk://"), s"$master does not start with zk://")
+
+  case class ZkUrl(auth: Option[String], servers: String, path: String)
+
+  implicit val mesosInfoRead: Reads[Protos.MasterInfo] = (
+    (JsPath \ "hostname").read[String] ~
+      (JsPath \ "port").read[Int] ~
+      (JsPath \ "id").read[String] ~
+      (JsPath \ "ip").read[Int]
+  ) { (hostname, port, id, ip) =>
+    Protos.MasterInfo.newBuilder().setHostname(hostname).setPort(port).setId(id).setIp(ip).build()
+  }
+
+  override def getMaster()(implicit ex: ExecutionContext): CompletionStage[URL] = {
+    val ZkUrl(_, servers, path) = parse() // TODO: use authentication.
+
+    val client = CuratorFrameworkFactory.newClient(servers, new RetryOneTime(100))
+    client.start()
+
+    if (!client.blockUntilConnected(
+        client.getZookeeperClient.getConnectionTimeoutMs,
+        java.util.concurrent.TimeUnit.MILLISECONDS)) {
+      throw new IllegalStateException("Failed to connect to Zookeeper. Will exit now.")
+    }
+
+    val asyncClient = AsyncCuratorFramework.wrap(client)
+
+    async {
+      val children = await(asyncClient.getChildren.forPath(path).toScala).asScala
+      val leader = children.filter(_.startsWith("json.info")).min
+
+      val leaderPath = s"$path/$leader"
+      logger.info(s"Connecting to Zookeeper at $servers and fetching Mesos master from $leaderPath.")
+
+      val bytes = await(asyncClient.getData.forPath(leaderPath).toScala)
+      logger.info(s"Mesos leader data: ${new String(bytes)}")
+
+      val masterInfo = Json.parse(bytes).as[Protos.MasterInfo]
+      // TODO: how do we know it's http or https.
+      val url = new URL(s"http://${masterInfo.getHostname}:${masterInfo.getPort}")
+      url
+    }.toJava
+  }
+
+  /** @return proper Zookeeper connection string as per {@link ZooKeeper#ZooKeeper(String, int, Watcher)} etc. */
+  def parse(): ZkUrl = {
+    // Strip leading zk://
+    val stripped = master.substring(5)
+
+    // Extract path
+    val pathIndex = stripped.indexOf('/')
+    val path = if (pathIndex < 0) "/" else stripped.substring(pathIndex, stripped.length)
+
+    // Find optional authentication
+    val endIndex = if (pathIndex < 0) stripped.length else pathIndex
+    stripped.substring(0, endIndex).split('@') match {
+      case Array(auth, servers) => ZkUrl(Some(auth), servers, path)
+      case Array(servers) => ZkUrl(None, servers, path)
+      case _ => throw new IllegalArgumentException(s"$master contained more than one authentication.")
+    }
+  }
+}
+
+case class Standalone(master: String) extends MasterDetector {
+  val url = if (master.startsWith("http") || master.startsWith("https")) new URL(master) else new URL(s"http://$master")
+
+  override def getMaster()(implicit ex: ExecutionContext): CompletionStage[URL] = Future.successful(url).toJava
+}
---
+++ mesos-client/src/test/scala/com/mesosphere/mesos/client/MasterDetectorTest.scala
@@ -0,0 +1,73 @@
+package com.mesosphere.mesos.client
+import com.mesosphere.utils.AkkaUnitTest
+import com.mesosphere.utils.mesos.{MesosClusterTest, MesosConfig}
+
+import scala.compat.java8.FutureConverters._
+
+class MasterDetectorTest extends AkkaUnitTest with MesosClusterTest {
+
+  override lazy val mesosConfig = MesosConfig(numMasters = 2)
+
+  "The master detector factory" should {
+    "detect Zookeeper" in {
+      MasterDetector("zk://host1:port1,host2:port2/path/to/master") shouldBe a[Zookeeper]
+    }
+
+    "detect Standalone" in {
+      MasterDetector("http://host1:5050") shouldBe a[Standalone]
+      MasterDetector("https://host1:5050") shouldBe a[Standalone]
+      MasterDetector("host1:5050") shouldBe a[Standalone]
+    }
+  }
+
+  "The Zookeeper Detector" should {
+    "extract the path and servers" in {
+      Given("the master string zk://host1:port1,host2:port2/path/to/master")
+      val master = "zk://host1:port1,host2:port2/path/to/master"
+
+      When("we parse it")
+      val result = Zookeeper(master).parse()
+
+      Then("the servers and path were extracted")
+      result.auth should be('empty)
+      result.servers should be("host1:port1,host2:port2")
+      result.path should be("/path/to/master")
+    }
+
+    "extract authentication" in {
+      Given("the master string zk://user:password@host1:port1,host2:port2/path/to/master")
+      val master = "zk://user:password@host1:port1,host2:port2/path/to/master"
+
+      When("we parse it")
+      val result = Zookeeper(master).parse()
+
+      Then("the servers and path were extracted")
+      result.auth.value should be("user:password")
+    }
+
+    "default the path to /" in {
+      Given("the master string zk://user:password@host1:port1,host2:port2")
+      val master = "zk://user:password@host1:port1,host2:port2"
+
+      When("we parse it")
+      val result = Zookeeper(master).parse()
+
+      Then("the path should default to /")
+      result.path should be("/")
+    }
+  }
+
+  "The master detector" should {
+    "detect the Mesos master" in {
+      Given(s"the Zookeeper server at $mesosMasterZkUrl and a detector")
+      val detector = MasterDetector(mesosMasterZkUrl)
+      val expectedLeader = mesosCluster.waitForLeader().toString
+
+      When("we detect the Mesos master")
+      val masterUrl = detector.getMaster().toScala.futureValue
+
+      Then("the URL is the same as our master")
+      masterUrl.toString should be(expectedLeader)
+    }
+  }
+}
