94c6330a5b556168317fefaee80a8d214ca8310d
Andreas Neumann <aneumann@mesosphere.com>
2019-10-30 15:24:48 +0100

    Fixed some timeout issues on startup Fixed issues with PodIds containing . Exit JVM if an error is thrown or the main loop exits

--- examples/keep-alive-framework/src/main/scala/com/mesosphere/usi/helloworld/KeepAlivePodSpecHelper.scala
+++ examples/keep-alive-framework/src/main/scala/com/mesosphere/usi/helloworld/KeepAlivePodSpecHelper.scala
@@ -5,11 +5,12 @@ import java.util.UUID
 import com.mesosphere.usi.core.models.resources.{ResourceType, ScalarRequirement}
 import com.mesosphere.usi.core.models.template.{RunTemplate, SimpleRunTemplateFactory}
 import com.mesosphere.usi.core.models.{PodId, RunningPodSpec}
+import com.typesafe.scalalogging.StrictLogging
 
 /**
   * This is a helper object that generates pod specs and snapshots.
   */
-object KeepAlivePodSpecHelper {
+object KeepAlivePodSpecHelper extends StrictLogging {
 
   val runSpec: RunTemplate = SimpleRunTemplateFactory(
     resourceRequirements = List(ScalarRequirement(ResourceType.CPUS, 0.001), ScalarRequirement(ResourceType.MEM, 32)),
@@ -18,8 +19,9 @@ object KeepAlivePodSpecHelper {
   )
 
   def generatePodSpec(): RunningPodSpec = {
-    val podId = PodId(s"hello-world.${UUID.randomUUID()}.1")
+    val podId = PodId(s"hello-world_${UUID.randomUUID()}_1")
 
+    logger.info(s"Generating PodSpec for '${podId}'")
     val podSpec = RunningPodSpec(id = podId, runSpec = runSpec)
     podSpec
   }
@@ -28,12 +30,13 @@ object KeepAlivePodSpecHelper {
     (1 to numberOfPods).map(_ => generatePodSpec())(collection.breakOut)
 
   def createNewIncarnationId(podId: PodId): PodId = {
-    val idAndIncarnation = """^(.+\..*)\.(\d+)$""".r
+    val idAndIncarnation = """^(.+_.*)_(\d+)$""".r
     val (podIdWithoutIncarnation, currentIncarnation) = podId.value match {
       case idAndIncarnation(id, inc) =>
         id -> inc.toLong
+      case _ => throw new IllegalArgumentException(s"Failed to create new incarnation id for ${podId.value}")
     }
-    PodId(s"$podIdWithoutIncarnation.${currentIncarnation + 1}")
+    PodId(s"${podIdWithoutIncarnation}_${currentIncarnation + 1}")
   }
 
 }
