9145841963f0e3271e0a6bf2ed91f579529ad2d0
Karsten Jeschkies <kjeschkies@mesosphere.com>
2019-11-23 02:32:32 +0100

    Define string attribute matcher for agents. (#129)
    * Define string attribute matcher for agents.
    
    * Convert Java hash map.

--- core-models/src/main/scala/com/mesosphere/usi/core/models/constraints/AgentAttributeFilter.scala
+++ core-models/src/main/scala/com/mesosphere/usi/core/models/constraints/AgentAttributeFilter.scala
@@ -1,9 +1,41 @@
 package com.mesosphere.usi.core.models.constraints
 
-import org.apache.mesos.v1.Protos
-import collection.JavaConverters._
+import java.util
 
+import org.apache.mesos.v1.Protos
+
+import collection.JavaConverters._
 import com.typesafe.scalalogging.StrictLogging
+
+/**
+  * An agent attribute filter that will only accept offers from agents that contain all required string attributes.
+  *
+  * The agent may contain more attributes.
+  *
+  * @param requiredAttributes The subset of agent attributes the agent *must* have.
+  */
+case class AgentStringAttributeFilter(requiredAttributes: Map[String, String]) extends AgentFilter with StrictLogging {
+
+  /*
+   * Java constructor.
+   */
+  def this(javaAttributes: util.HashMap[String, String]) = {
+    this(requiredAttributes = javaAttributes.asScala.toMap)
+  }
+
+  override def apply(offer: Protos.Offer): Boolean = {
+    val agentAttributes: Map[String, String] = offer.getAttributesList.asScala
+      .filter(_.getType == Protos.Value.Type.TEXT)
+      .map { attribute =>
+        attribute.getName -> attribute.getText.getValue
+      }(collection.breakOut)
+
+    requiredAttributes.forall {
+      case (key, value) =>
+        agentAttributes.get(key).contains(value)
+    }
+  }
+}
 
 case class AgentAttributeFilter(attributes: Map[String, Any]) extends AgentFilter with StrictLogging {
 
