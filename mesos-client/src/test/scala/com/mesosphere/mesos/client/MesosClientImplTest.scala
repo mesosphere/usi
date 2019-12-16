0554bb4b87fe610f223efa45f0239e688f733aaa
Karsten Jeschkies <kjeschkies@mesosphere.com>
2019-12-16 11:59:38 +0100

    Fail Mesos client if master version is too old. (#132)
    Summary:
    The Jenkins plugin reported `HTTP 400` errors. To ensure that the Mesos
    master version is compatible we are checking it during the client
    creation.

---
+++ mesos-client/src/test/scala/com/mesosphere/mesos/client/MesosClientImplTest.scala
@@ -0,0 +1,42 @@
+package com.mesosphere.mesos.client
+import java.net.URL
+
+import akka.stream.KillSwitches
+import akka.stream.scaladsl.Source
+import akka.util.Timeout
+import com.mesosphere.utils.AkkaUnitTest
+import org.apache.mesos.v1.Protos.{FrameworkID, MasterInfo}
+import org.apache.mesos.v1.scheduler.Protos.Event
+
+import scala.concurrent.duration._
+
+class MesosClientImplTest extends AkkaUnitTest {
+
+  implicit val askTimeout = Timeout(1.minute)
+
+  "Mesos client" should {
+    "fail if the provided Mesos version is incompatible" in {
+      val masterInfo = MasterInfo.newBuilder().setVersion("1.8.0").setId("unique").setIp(42).setPort(0).build()
+      val frameworkId = FrameworkID.newBuilder().setValue("framework").build()
+      val subscribedEvent = Event.Subscribed.newBuilder().setMasterInfo(masterInfo).setFrameworkId(frameworkId).build()
+      val sharedKillSwitch = KillSwitches.shared("mesos-client-killswitch")
+      val session = Session(new URL("http://localhost"), "stream-id")
+
+      a[IllegalArgumentException] should be thrownBy {
+        new MesosClientImpl(null, sharedKillSwitch, subscribedEvent, session, Source.empty)
+      }
+    }
+
+    "pass if the provided Mesos version is compatible" in {
+      val masterInfo = MasterInfo.newBuilder().setVersion("1.9.0").setId("unique").setIp(42).setPort(0).build()
+      val frameworkId = FrameworkID.newBuilder().setValue("framework").build()
+      val subscribedEvent = Event.Subscribed.newBuilder().setMasterInfo(masterInfo).setFrameworkId(frameworkId).build()
+      val sharedKillSwitch = KillSwitches.shared("mesos-client-killswitch")
+      val session = new Session(new URL("http://localhost"), "stream-id")
+
+      noException should be thrownBy {
+        new MesosClientImpl(null, sharedKillSwitch, subscribedEvent, session, Source.empty)
+      }
+    }
+  }
+}
