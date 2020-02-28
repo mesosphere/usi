33447724e486cf4c8841d82cf1b381428f6f2013
Karsten Jeschkies <kjeschkies@mesosphere.com>
2020-02-28 11:49:56 +0100

    Enable user to use Docker entrypoint. (#140)
    Summary:
    This enables USI users to specify whether a command is executed in shell
    or passed as an argument to the entrypoint of a Docker image.
    
    Relates to https://github.com/mesosphere/dcos-jenkins-service/pull/319.
    
    JIRA issues: DCOS_OSS-5937

--- core-models/src/main/scala/com/mesosphere/usi/core/models/template/SimpleRunTemplateFactory.scala
+++ core-models/src/main/scala/com/mesosphere/usi/core/models/template/SimpleRunTemplateFactory.scala
@@ -1,29 +1,82 @@
 package com.mesosphere.usi.core.models.template
 
+import java.util.Optional
+
 import com.mesosphere.usi.core.models.resources.ResourceRequirement
-import com.mesosphere.usi.core.models.{TaskName, TaskBuilder}
+import com.mesosphere.usi.core.models.{TaskBuilder, TaskName}
 import org.apache.mesos.v1.{Protos => Mesos}
 
+import scala.annotation.varargs
 import scala.collection.JavaConverters._
 
 object SimpleRunTemplateFactory {
+
+  sealed trait Command {
+    def build(): Mesos.CommandInfo.Builder
+  }
+
+  /**
+    * Use `/bin/sh -c` to execute passed command.
+    *
+    * @param cmd The shell command that is executed.
+    */
+  case class Shell(cmd: String) extends Command {
+    def build(): Mesos.CommandInfo.Builder = {
+      Mesos.CommandInfo
+        .newBuilder()
+        .setShell(true)
+        .setValue(cmd)
+    }
+  }
+
+  /**
+    * Use the entrypoint of the Docker image to execute the arguments. If the argument list is empty `CMD` from the
+    * Docker image is called.
+    *
+    * @param args A list of arguments passed.
+    */
+  class DockerEntrypoint private (args: Seq[String]) extends Command {
+    def build(): Mesos.CommandInfo.Builder = {
+      val builder = Mesos.CommandInfo
+        .newBuilder()
+        .setShell(false)
+
+      args.zipWithIndex.foreach { case (arg, index) => builder.setArguments(index, arg) }
+
+      builder
+    }
+  }
+
+  object DockerEntrypoint {
+
+    /** Factory method for [[com.mesosphere.usi.core.models.template.SimpleRunTemplateFactory.DockerEntrypoint]]; Scala Api. */
+    def apply(args: Seq[String]) = new DockerEntrypoint(args)
+
+    /** Factory method for [[com.mesosphere.usi.core.models.template.SimpleRunTemplateFactory.DockerEntrypoint]]; Java Api. */
+    @varargs
+    def create(args: String*) = new DockerEntrypoint(args)
+  }
 
   /**
     * This task info builder is used by the [[LegacyLaunchRunTemplate]]. I launches a simple pod.
     *
     * @param resourceRequirements The resources required for the pod.
-    * @param shellCommand The command that is executed.
+    * @param command The command that is executed.
     * @param role The Mesos role used.
     * @param fetch The artifacts that are fetched by Mesos.
     * @param dockerImageName The optional Docker image the task will run in.
     */
-  case class SimpleTaskInfoBuilder(
-      resourceRequirements: Seq[ResourceRequirement],
-      shellCommand: String,
+  class SimpleTaskInfoBuilder private (
+      val resourceRequirements: Seq[ResourceRequirement],
+      command: Command,
       role: String,
-      fetch: Seq[FetchUri] = Seq.empty,
-      dockerImageName: Option[String] = None)
+      fetch: Seq[FetchUri],
+      dockerImageName: Option[String])
       extends TaskBuilder {
+
+    if (command.isInstanceOf[DockerEntrypoint]) {
+      assert(dockerImageName.isDefined, "The default entrypoint can only be used with a Docker image.")
+    }
 
     override def buildTask(
         taskInfoBuilder: Mesos.TaskInfo.Builder,
@@ -42,12 +95,8 @@ object SimpleRunTemplateFactory {
         }
         fetchBuilder.build()
       }
-      taskInfoBuilder.setCommand(
-        Mesos.CommandInfo
-          .newBuilder()
-          .setShell(true)
-          .setValue(shellCommand)
-          .addAllUris(uris.asJava))
+
+      taskInfoBuilder.setCommand(command.build().addAllUris(uris.asJava))
 
       dockerImageName.foreach { name =>
         taskInfoBuilder.setContainer(
@@ -63,6 +112,37 @@ object SimpleRunTemplateFactory {
                   .setDocker(Mesos.Image.Docker.newBuilder().setName(name))))
             .build())
       }
+    }
+  }
+
+  object SimpleTaskInfoBuilder {
+
+    def apply(
+        resourceRequirements: Seq[ResourceRequirement],
+        command: Command,
+        role: String,
+        fetch: Seq[FetchUri],
+        dockerImageName: Option[String]): SimpleTaskInfoBuilder = {
+      new SimpleTaskInfoBuilder(resourceRequirements, command, role, fetch, dockerImageName)
+    }
+
+    def apply(
+        resourceRequirements: Seq[ResourceRequirement],
+        shellCommand: String,
+        role: String,
+        fetch: Seq[FetchUri] = Seq.empty,
+        dockerImageName: Option[String] = None): SimpleTaskInfoBuilder =
+      apply(resourceRequirements, Shell(shellCommand), role, fetch, dockerImageName)
+
+    /** Factory method for [[com.mesosphere.usi.core.models.template.SimpleRunTemplateFactory.SimpleTaskInfoBuilder]]; Java Api. */
+    def create(
+        resourceRequirements: java.util.List[ResourceRequirement],
+        command: Command,
+        role: String,
+        fetch: java.util.List[FetchUri],
+        dockerImageName: Optional[String]): SimpleTaskInfoBuilder = {
+      val image = if (dockerImageName.isPresent) Some(dockerImageName.get()) else None
+      apply(resourceRequirements.asScala, command, role, fetch.asScala, image)
     }
   }
 
@@ -83,10 +163,5 @@ object SimpleRunTemplateFactory {
       dockerImageName: Option[String] = None): RunTemplate =
     new LegacyLaunchRunTemplate(
       role,
-      new SimpleTaskInfoBuilder(
-        resourceRequirements: Seq[ResourceRequirement],
-        shellCommand,
-        role,
-        fetch,
-        dockerImageName))
+      SimpleTaskInfoBuilder(resourceRequirements: Seq[ResourceRequirement], shellCommand, role, fetch, dockerImageName))
 }
